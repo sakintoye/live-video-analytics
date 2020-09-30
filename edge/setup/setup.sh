a42beb25cd1485faaa42bfa7a8c1cae3d4a86b9b
Ola Akintoye <3765280+sakintoye@users.noreply.github.com>
2020-09-30 11:29:53 -0700

    Update setup.sh

--- edge/setup/setup.sh
+++ edge/setup/setup.sh
@@ -18,7 +18,7 @@ BLUE='\033[1;34m'
 NC='\033[0m' # No Color
 
 # script configuration
-BASE_URL='https://raw.githubusercontent.com/Azure/live-video-analytics/master/edge/setup' # location of remote files used by the script
+BASE_URL='https://raw.githubusercontent.com/sakintoye/live-video-analytics/master/edge/setup' # location of remote files used by the script
 DEFAULT_REGION='centralus'
 ENV_FILE='edge-deployment/.env'
 APP_SETTINGS_FILE='appsettings.json'
